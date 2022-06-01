0aa25987e872e89aa47e5e25a24cd4b58dbe566c
Han Byul <yahb9@naver.com>
2022-06-02 02:22:32 +0900

    Feat: MakeTodo process board 생성

--- src/routes/MakeTodo/index.tsx
+++ src/routes/MakeTodo/index.tsx
@@ -1,5 +1,27 @@
+import NavBar from 'components/NavBar'
+
+import styles from './makeTodo.module.scss'
+
 const MakeTodo = () => {
-  return <div>Maketodo</div>
+  const processList = ['TODO', 'DOING', 'DONE']
+
+  return (
+    <div className={styles.container}>
+      <NavBar />
+      <main>
+        {processList.map((process) => {
+          return (
+            <div key='process-process' className={styles.processBox}>
+              <div className={styles.processTop}>
+                <div>{process}</div>
+                <div>+</div>
+              </div>
+            </div>
+          )
+        })}
+      </main>
+    </div>
+  )
 }
 
 export default MakeTodo
