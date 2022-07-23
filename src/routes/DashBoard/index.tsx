5f459d7d1a9f68f051bbf7f963e12ddefdab95ff
Han Byul <yahb9@naver.com>
2022-07-23 23:59:48 +0900

    Feat: Create nickName input Container

--- src/routes/DashBoard/index.tsx
+++ src/routes/DashBoard/index.tsx
@@ -1,18 +1,51 @@
-import { useState } from 'react'
+import { useState, ChangeEvent, FormEvent } from 'react'
 import ReactDatePicker from 'react-datepicker'
 
 import CustomDateInput from './CustomDateInput'
 
+import { EditIcon } from 'assets/svgs'
+import styles from './dashBoard.module.scss'
+
 const DashBoard = () => {
   const [startDate, setStartDate] = useState<Date | null>(new Date())
+  const [nickName, setNickName] = useState('')
+  const [editNickName, setEditNickName] = useState(true)
+
+  const handleNickNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNickName(e.currentTarget.value)
+  }
+
+  const handleNickNameSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setNickName(nickName)
+    setEditNickName(false)
+  }
+
+  const handleNickNameEditClick = () => {
+    setEditNickName(true)
+  }
 
   return (
-    <ReactDatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      dateFormat='eee, dd MMMM yyyy'
-      customInput={<CustomDateInput />}
-    />
+    <header className={styles.header}>
+      <div className={styles.nickNameContainer}>
+        {editNickName ? (
+          <form className={styles.nickNameForm} onSubmit={handleNickNameSubmit}>
+            <input value={nickName} onChange={handleNickNameChange} placeholder='Please put your nickname' />
+          </form>
+        ) : (
+          <div className={styles.nickName}>
+            Welcome back <strong>{nickName}</strong>!
+          </div>
+        )}
+        <EditIcon className={styles.editIcon} onClick={handleNickNameEditClick} />
+      </div>
+      <ReactDatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat='eee, dd MMMM yyyy'
+        customInput={<CustomDateInput />}
+      />
+    </header>
   )
 }
 
