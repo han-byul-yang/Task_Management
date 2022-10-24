f9167e0bfb34a6306577b030a66024f00a22101e
Han Byul <yahb9@naver.com>
2022-10-24 17:46:44 +0900

    Feat: filterTarget에서 filterType에 따라 filterKeyword로 카드를 필터링하는 filterContents utils 생성

---
+++ src/utils/filterContents.ts
@@ -0,0 +1,40 @@
+import { IBoardTaskList } from 'types/taskType'
+
+interface IFilterTarget {
+  [key: string]: { [taskKey: string]: string | string[] }[]
+}
+
+const filterContents = (filterType: string, filterKeyword: string, filterTarget: any) => {
+  const filteredTasksObject: any = {}
+
+  const taskType = {
+    제목: 'taskTitle',
+    카테고리: 'categoryList',
+    내용: 'description',
+  }[filterType]
+
+  if (filterType) {
+    Object.keys(filterTarget).forEach((key: string) => {
+      filteredTasksObject[key] = filterTarget[key].filter((task: any) =>
+        task[taskType!].includes(filterKeyword.substring(filterType.length + 2))
+      )
+    })
+  }
+
+  if (!filterType) {
+    Object.keys(filterTarget).forEach((key: string) => {
+      filteredTasksObject[key] = filterTarget[key].filter(
+        (task: any) =>
+          task.taskTitle.includes(filterKeyword) ||
+          task.categoryList.includes(filterKeyword) ||
+          task.description.includes(filterKeyword)
+      )
+    })
+  }
+
+  return filteredTasksObject
+}
+
+export default filterContents
+
+// any type
