d61c172bddda1a06f131cf8d56840021deafbc08
Han Byul <yahb9@naver.com>
2022-10-24 23:31:30 +0900

    Rename: CustomButton -> CustomDateButton

--- src/components/Modal/components/Schedule/CustomDateButton.tsx
+++ src/components/Modal/components/Schedule/CustomDateButton.tsx
@@ -3,8 +3,8 @@ import { forwardRef, LegacyRef } from 'react'
 import { ClockIcon } from 'assets/svgs'
 import styles from './schedule.module.scss'
 
-export const CustomButton = forwardRef(({ value, onClick }: any, ref: LegacyRef<HTMLButtonElement> | undefined) => (
-  <div className={styles.dateBtn}>
+export const CustomDateButton = forwardRef(({ value, onClick }: any, ref: LegacyRef<HTMLButtonElement> | undefined) => (
+  <div className={styles.dateButton}>
     <ClockIcon onClick={onClick} />
     <button type='button' onClick={onClick} ref={ref}>
       <div>{value}</div>
@@ -12,6 +12,4 @@ export const CustomButton = forwardRef(({ value, onClick }: any, ref: LegacyRef<
   </div>
 ))
 
-CustomButton.displayName = 'customButton'
-
-// component 이름 수정
+CustomDateButton.displayName = 'customButton'
