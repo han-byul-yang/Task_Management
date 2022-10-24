f38652655a448473571cd8bc3e55912c7d2b99de
Han Byul <yahb9@naver.com>
2022-10-24 21:23:36 +0900

    Fix: handleSubmitTaskClick 함수 분리 및 코드 정리

--- src/components/Modal/AddTaskModal/index.tsx
+++ src/components/Modal/AddTaskModal/index.tsx
@@ -1,9 +1,7 @@
-import { Dispatch, SetStateAction, useState } from 'react'
-import { useMount } from 'react-use'
+import { useState } from 'react'
 import { useRecoilState, useSetRecoilState } from 'recoil'
 
 import { isOpenAddTaskModalAtom, taskAtom, tasksAtom } from 'store/atoms'
-import { ITask } from 'types/taskType'
 import Title from '../components/Title'
 import Category from '../components/Category'
 import Description from '../components/Description'
@@ -16,122 +14,66 @@ import styles from './addTaskModal.module.scss'
 
 interface IDashBoardModalProps {
   boardProcessName: string
-  todo?: ITask
 }
 
-const AddTaskModal = ({ boardProcessName, todo }: IDashBoardModalProps) => {
-  const [taskTitle, setTaskTitle] = useState('')
-  const [categoryList, setCategoryList] = useState<string[]>([])
+const AddTaskModal = ({ boardProcessName }: IDashBoardModalProps) => {
   const [date, setDate] = useState<(Date | null)[]>([])
-  const [image, setImage] = useState<string | ArrayBuffer | null | undefined>()
-  const [description, setDescription] = useState<string>('')
   const [noTitle, setNoTitle] = useState(false)
   const [noCategory, setNoCategory] = useState(false)
-  const [boardsTasks, setBoardsTasks] = useRecoilState(tasksAtom)
+  const setBoardsTasks = useSetRecoilState(tasksAtom)
   const [task, setTask] = useRecoilState(taskAtom)
   const [isOpenAddTaskModal, setIsOpenAddTaskModal] = useRecoilState(isOpenAddTaskModalAtom)
 
-  /*  useMount(() => {
-    if (!todo) return
-    if (todo) {
-      setTaskTitle(todo.taskTitle)
-      setCategoryList(todo.categoryList)
-      setDate([new Date(), null])
-      setImage(todo?.image.url)
-      setDescription(todo?.description)
-    }
-  })
-*/
-  const handleCloseModal = () => {
-    setIsOpenAddTaskModal((prevState) => ({ ...prevState, isOpen: false }))
+  const resetTask = () => {
+    setTask({
+      id: new Date(),
+      process: '',
+      taskTitle: '',
+      categoryList: [],
+      date: [],
+      image: { name: '', url: '' },
+      description: '',
+    })
   }
 
-  /* const handleCreateTaskClick = () => {
-    if (task !== '' && categoryList.length !== 0) {
-      if (todo) {
-        const todoIdArray = boardsTasks[todo.process].map((todos) => todos.id)
-        const todoIdIndex = todoIdArray.indexOf(todo.id)
-        const copyArray = [...boardsTasks[todo.process]]
-        copyArray.splice(todoIdIndex, 1, {
-          id: todo.id,
-          process: todo.process,
-          taskTitle: task,
-          categoryList,
-          date,
-          image: { url: image, name: '' },
-          description,
-        })
-        setboardsTasks((oldTodos) => {
-          return {
-            ...oldTodos,
-            [todo.process]: [...copyArray],
-          }
-        })
-      } else {
-        setboardsTasks((oldTodos) => {
-          return {
-            ...oldTodos,
-            [boardProcessName]: [
-              ...oldTodos[boardProcessName],
-              {
-                id: new Date(),
-                process: boardProcessName,
-                taskTitle: task,
-                categoryList,
-                date,
-                image: { url: image, name: '' },
-                description,
-              },
-            ],
-          }
-        })
+  const addTaskToBoard = () => {
+    setBoardsTasks((prevTasks) => ({
+      ...prevTasks,
+      [boardProcessName]: [...prevTasks[boardProcessName], { ...task, id: new Date(), process: boardProcessName }],
+    }))
+  }
+
+  const editTaskToBoard = () => {
+    setBoardsTasks((prevTasks) => {
+      const tempPrevTasks = [...prevTasks[boardProcessName]]
+      const taskIdList = tempPrevTasks.map((prevTask) => prevTask.id)
+      tempPrevTasks.splice(taskIdList.indexOf(task.id), 1, task)
+      return {
+        ...prevTasks,
+        [boardProcessName]: tempPrevTasks,
       }
+    })
+  }
 
-      handleCloseModal()
+  const handleCloseModal = () => {
+    setIsOpenAddTaskModal((prevState) => ({ ...prevState, isOpen: false }))
+    if (isOpenAddTaskModal.type === 'edit') {
+      resetTask()
     }
+  }
 
-    if (task === '') {
-      setNoTask(true)
-    } else {
-      setNoTask(false)
-    }
-    if (categoryList.length === 0) {
-      setNoCategory(true)
-    } else {
-      setNoCategory(false)
-    }
-  } */
   const handleSubmitTaskClick = () => {
     if (!task.taskTitle || !task.categoryList.length) {
       if (!task.taskTitle) setNoTitle(true)
       if (!task.categoryList.length) setNoCategory(true)
     } else {
       if (isOpenAddTaskModal.type === 'add') {
-        setBoardsTasks((prevTasks) => ({
-          ...prevTasks,
-          [boardProcessName]: [...prevTasks[boardProcessName], { ...task, id: new Date(), process: boardProcessName }],
-        }))
+        addTaskToBoard()
       }
       if (isOpenAddTaskModal.type === 'edit') {
-        setBoardsTasks((prevTasks) => {
-          const tempPrevTasks = [...prevTasks[boardProcessName]]
-          const taskIdList = tempPrevTasks.map((prevTask) => prevTask.id)
-          tempPrevTasks.splice(taskIdList.indexOf(task.id), 1, task)
-          return {
-            ...prevTasks,
-            [boardProcessName]: tempPrevTasks,
-          }
-        })
+        editTaskToBoard()
       }
-      setTask({
-        id: new Date(),
-        process: '',
-        taskTitle: '',
-        categoryList: [],
-        date: [],
-        image: { name: '', url: '' },
-        description: '',
-      })
+      resetTask()
       setIsOpenAddTaskModal((prevState) => ({ ...prevState, isOpen: false }))
     }
   }
