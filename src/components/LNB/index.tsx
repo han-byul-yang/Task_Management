915c98cbcc920d4d660d50d720575f9696617bf2
Han Byul <yahb9@naver.com>
2022-10-26 16:20:30 +0900

    Feat: tablet 크기 화면 시 띄워줄 board LNB 컴포넌트 생성

---
+++ src/components/LNB/index.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+
+import { boardProcessAtom } from 'store/atoms'
+
+import styles from './lnb.module.scss'
+
+const LNB = () => {
+  const boardProcessList = useRecoilValue(boardProcessAtom)
+
+  return (
+    <nav className={styles.lnb}>
+      <ul className={styles.navList}>
+        {boardProcessList.map((process) => {
+          const navigationKey = `navigation-${process}`
+          return (
+            <NavLink
+              key={navigationKey}
+              to={`${process}`}
+              className={({ isActive }) => (isActive ? `${styles.activatedLink}` : `${styles.nonActivatedLink}`)}
+            >
+              <li className={styles.navItem}>{process}</li>
+            </NavLink>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default LNB
