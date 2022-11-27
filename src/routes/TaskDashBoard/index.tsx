c4ccad46999c45b4ec83d1716823b8da4a3006fd
Han Byul <yahb9@naver.com>
2022-11-28 01:51:15 +0900

    Feat: Modal lazy loading

--- src/routes/TaskDashBoard/index.tsx
+++ src/routes/TaskDashBoard/index.tsx
@@ -1,14 +1,19 @@
 import { useRecoilValue } from 'recoil'
 
 import { isOpenModalAtom } from 'store/atoms'
-import WriteTaskModal from 'components/Modal/WriteTaskModal'
+// import WriteTaskModal from 'components/Modal/WriteTaskModal'
 import ModalPortal from 'components/Modal/ModalPortal'
 import SearchInput from './SearchInput'
 import BoardsContainer from './BoardsContainer'
-import WriteBoardModal from 'components/Modal/WriteBoardModal'
-import NoticeModal from 'components/Modal/NoticeModal'
+// import WriteBoardModal from 'components/Modal/WriteBoardModal'
+// import NoticeModal from 'components/Modal/NoticeModal'
 
 import styles from './taskDashboard.module.scss'
+import { lazy } from 'react'
+
+const WriteTaskModal = lazy(() => import('components/Modal/WriteTaskModal'))
+const WriteBoardModal = lazy(() => import('components/Modal/WriteBoardModal'))
+const NoticeModal = lazy(() => import('components/Modal/NoticeModal'))
 
 const TaskDashboard = () => {
   const isOpenModal = useRecoilValue(isOpenModalAtom)
